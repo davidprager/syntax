b8251aa3707aca0f152ffc5d48e66fbdf709194c
davidprager <david_prager@yahoo.com>
2021-12-04 20:06:14 -0500

    Initial commit

---
+++ syntax.js
@@ -0,0 +1,33 @@
+/* This file will be used to test some basic JavaScript syntax */
+
+"use strict"
+const PI = 3.14;
+// This function displays the result of an arithmetic expression
+function numbers() {
+    // Enter the expression from the powerpoint
+}
+
+// This function calculates the area of a circle
+/*function numbers() {
+
+}*/
+
+// This function calculates the area of two circles
+// and then adds them together
+/*function numbers() {
+
+}*/
+
+// This function calculates the area of two circles
+// by calling the calculateArea function
+// and then adds the areas together
+/*function numbers() {
+
+}*/
+
+// This function returns the area of a circle
+// It takes the circle's radius as an input argument
+function calculateArea(radius){
+
+}
+
